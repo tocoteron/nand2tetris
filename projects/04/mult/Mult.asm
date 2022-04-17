f0c8df6baaef3c39f7727215c4e3bea7512ef76d
Tensei Ida <tocoteron@gmail.com>
2022-04-17 17:40:34 +0900

    Implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,28 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @R0
+    D=M
+    @i
+    M=D
+    @R2
+    M=0
+(LOOP)
+    // if i <= 0 then break
+    @i
+    D=M
+    @END
+    D;JLE
+    // R2 += R1
+    @R1
+    D=M
+    @R2
+    M=D+M
+    // i--;
+    @i
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
